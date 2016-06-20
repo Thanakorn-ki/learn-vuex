acd8c38f188cb1353bfc6f4eacc6596a6f20337c
Nati Namvong <nati@flyingcomma.com>
2016-06-21 01:50:03 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Display></Display>
+    <Increment></Increment>
+  </div>
+</template>
+
+<script>
+import Display from './components/Display.vue'
+import Increment from './components/Increment.vue'
+import store from './vuex/store' // import the store we just created
+
+export default {
+  components: {
+    Display,
+    Increment
+  },
+  store // make this and all child components aware of the new store
+}
+</script>
